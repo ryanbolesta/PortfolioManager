abb7ae0a8bc691e06f6cf416f3229ebe5bb3c65f
ryanbolesta <ryanbolesta@Ryans-MacBook-Pro-2.local>
2019-07-04 12:56:50 -0400

    Initial commit of Bobs portfolio

---
+++ robertbolesta/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+          Robert Bolesta
+      </p>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
