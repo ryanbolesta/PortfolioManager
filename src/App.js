40a4463d1dfbea328dcee6171567dc2337a9e9de
ryanbolesta <ryanbolesta@Ryans-MacBook-Pro-2.local>
2019-07-15 19:32:04 -0400

    testing heroku deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AdminForm from './Components/AdminForm.js'
+
+function App() {
+  return (
+    <div>
+      <AdminForm name='Robert'/>
+    </div>
+  );
+}
+
+export default App;
